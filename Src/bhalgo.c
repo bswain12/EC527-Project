7ac095c3b508d6287c7eb7dfa3eec028ca50a9ee
Brandon Swain <bswain@bu.edu>
2022-03-31 12:56:41 -0400

    Adding more info about the tree structure and the nodes

---
+++ Src/bhalgo.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+/* Useful for optimization later */
+
+//#include <omp.h>
+//#include <pthread.h>
+
+/* We need to create a quadtree structure occupied by "nodes" or "points" which individually have
+x and y coordinates and a Mass quantity. */
+struct node {
+
+
+};
